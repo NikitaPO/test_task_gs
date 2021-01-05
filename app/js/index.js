2090c424d0c1bac6cc919f4de703e1867e63d5be
NikitaPO <gigzim345@gmail.com>
2021-01-05 14:55:50 +0300

    fixed products data and product cards

--- app/js/index.js
+++ app/js/index.js
@@ -1,213 +1,66 @@
 const productsData = [
   {
     productId: "8b051a82-e051-11e4-afe5-00259036a192",
-    code: "00000142709",
-    title:
-      "Плитка облицовочная Сан-Ремо 7 200х200х7 мм белая (26 шт=1.04 кв.м)",
-    description:
-      "Предназначена для облицовки стен внутри жилых (кухня, ванная комната, туалет и пр.) и коммерческих (магазины, кафе, отели и пр.) помещений.\n\nТон (оттенок) может отличаться от партии к партии.\n\nСостав: огнеупорная глина, пылевой шпат, кварцевый песок, каолин, красители, фритта.\n\nСтепень блеска: глянцевый.\nКоличество на поддоне: 96 коробок.\nПроизводитель: ОАО «Керамин», Беларусь.\n\nСопутствующие: клей для плитки, крестики, затирка для швов, плиткорез.\n",
-    primaryImageUrl:
-      "//tdp.ru/images/p/cd9/3ef76-fbad-11e4-afe5-00259036a192.jpg",
-    assocProducts: "клей для плитки;\nкрестики;\nзатирка для швов;\nплиткорез;",
-    weight: 13600,
-    unit: "упак.",
-    unitFull: "упаковка",
-    unitRatio: 1,
-    unitAlt: "м. кв.",
-    unitRatioAlt: 0.9615,
-    unitFullAlt: "метр квадратный",
     priceRetail: 593,
     priceRetailAlt: 570.1695,
     priceGold: 576,
     priceGoldAlt: 553.824,
-    bonusAmount: 0,
-    hasAlternateUnit: true,
-    isActive: true,
-    modified: "2016-10-27 09:58:16",
   },
   {
     productId: "dded02e3-135e-11e5-b9a9-00259036a192",
-    code: "00000145471",
-    title:
-      "Керамогранит Zula 600х600х10 мм бежевый травертин полированный (4 шт= 1.44 кв.м)",
-    description:
-      "Предназначен для укладки внутри помещений, в том числе с  высокой проходимостью, а так же для наружных работ, для облицовки фасадов и др. \n\nТон (оттенок) и калибр (точность размеров по длине и ширине) может отличаться от партии к партии.\n\nСостав: огнеупорная глина, кварцевый песок, пигменты.\n\nСтепень блеска: матовый.\nКоличество на поддоне: 40 коробок.\nПроизводитель: Китай.\n\nСопутствующие: клей для керамогранита; крестики; затирка для швов; плиткорез; крестики для плитки; затирка для швов; плиткорез;\n\n",
-    primaryImageUrl:
-      "//tdp.ru/images/p/dc8/f086d-3073-11e5-b9a9-00259036a192.jpg",
-    assocProducts:
-      "клей для керамогранита; крестики; затирка для швов; плиткорез;; крестики для плитки; затирка для швов; плиткорез;",
-    weight: 33000,
-    unit: "упак.",
-    unitFull: "упаковка",
-    unitRatio: 1,
-    unitAlt: "м. кв.",
-    unitRatioAlt: 0.69444,
-    unitFullAlt: "метр квадратный",
     priceRetail: 1336,
     priceRetailAlt: 927.7718,
     priceGold: 1283,
     priceGoldAlt: 890.9665,
-    bonusAmount: 0,
-    hasAlternateUnit: true,
-    isActive: true,
-    modified: "2016-11-02 12:57:00",
   },
   {
     productId: "5a26045c-0a0c-11e6-bed3-00259036a192",
-    code: "00000156521",
-    title: "Плитка бордюр 400х30 мм Вилланелла белый",
-    description:
-      "Предназначена для использования в качестве декоративного элемента, применение которого позволяет украсить облицовываемую поверхность разнообразными узорами и орнаментами.\n \nТон (оттенок) может отличаться от партии к партии.\n\nСостав: огнеупорная глина, песок, известняк, пигменты.\n\nСтепень блеска: глянцевый\nКоличество в упаковке: 22 шт.\nПроизводитель: KERAMA MARAZZI, Россия\n\nСопутствующие: клей для плитки, крестики, затирка для швов, плиткорез.\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n",
-    primaryImageUrl:
-      "//tdp.ru/images/p/c57/352c8-0a0c-11e6-bed3-00259036a192.jpg",
-    assocProducts: "клей для плитки;\nкрестики;\nзатирка для швов;\nплиткорез;",
-    weight: 300,
-    unit: "шт.",
-    unitFull: "штука",
-    unitRatio: 1,
-    unitAlt: "шт.",
-    unitRatioAlt: 1,
-    unitFullAlt: "штука",
     priceRetail: 115,
     priceRetailAlt: 115,
     priceGold: 111,
     priceGoldAlt: 111,
     bonusAmount: 0,
-    hasAlternateUnit: true,
-    isActive: true,
-    modified: "2016-10-27 10:05:08",
   },
   {
     productId: "19d7362b-70d6-11e5-9d89-00259036a192",
-    code: "00000148058",
-    title: "Плитка настенная Natali 250х330х7 мм белый (18 шт=1.49 кв.м)",
-    description:
-      "Предназначена для облицовки стен внутри жилых (кухня, ванная комната, туалет и пр.) и коммерческих (магазины, кафе, отели и пр.) помещений.\n\nТон (оттенок) может отличаться от партии к партии.\nСостав: огнеупорная глина, песок, известняк, пигменты. \n\nСтепень блеска:матовая\nКоличество на поддоне: 54 коробки .\nПроизводитель: LASSElSBERGER, Россия\n\nСопутствующие: клей для плитки, крестики, затирка для швов, плиткорез.\n\n",
-    primaryImageUrl:
-      "//tdp.ru/images/p/96f/31ac9-70d6-11e5-9d89-00259036a192.JPG",
-    assocProducts: "клей для плитки;\nкрестики;\nзатирка для швов;\nплиткорез;",
-    weight: 17900,
-    unit: "упак.",
-    unitFull: "упаковка",
-    unitRatio: 1,
-    unitAlt: "м. кв.",
-    unitRatioAlt: 0.67114,
-    unitFullAlt: "метр квадратный",
     priceRetail: 743,
     priceRetailAlt: 498.657,
     priceGold: 714,
     priceGoldAlt: 479.194,
-    bonusAmount: 0,
-    hasAlternateUnit: true,
-    isActive: true,
-    modified: "2016-10-27 09:53:49",
   },
   {
     productId: "46751ca7-ecdd-11e4-afe5-00259036a192",
-    code: "00000144103",
-    title: "Плитка облицовочная Мираж 200х300х7 мм верх (24 шт=1.44 кв.м)",
-    description:
-      "Предназначена для облицовки стен внутри жилых (кухня, ванная комната, туалет и пр.) и коммерческих (магазины, кафе, отели и пр.) помещений.\n\nТон (оттенок) может отличаться от партии к партии.\n\nСостав: огнеупорная глина, песок, известняк, пигменты.\n\nСтепень блеска: глянцевый.\nКоличество на поддоне: 54 коробок.\nПроизводитель: ОAО «ВКЗ», Россия.\n\nСопутствующие: клей для плитки, крестики, затирка для швов, плиткорез.\n\n",
-    primaryImageUrl:
-      "//tdp.ru/images/p/19b/1dd48-fbc6-11e4-afe5-00259036a192.jpg",
-    assocProducts: "клей для плитки;\nкрестики;\nзатирка для швов;\nплиткорез;",
-    weight: 16490,
-    unit: "упак.",
-    unitFull: "упаковка",
-    unitRatio: 1,
-    unitAlt: "м. кв.",
-    unitRatioAlt: 0.69444,
-    unitFullAlt: "метр квадратный",
     priceRetail: 409,
     priceRetailAlt: 284.026,
     priceGold: 397,
     priceGoldAlt: 275.6927,
-    bonusAmount: 0,
-    hasAlternateUnit: true,
-    isActive: true,
-    modified: "2016-10-27 09:58:22",
   },
   {
     productId: "1f2f144c-3fea-11e5-b9a9-00259036a192",
-    code: "00000146668",
-    title: "Плитка декор Relax 250х400х8 мм зеленый №2 494321",
-    description:
-      "ПАННО СОСТОИТ из 4 позиций:\nПлитка декор 250х400х8 мм Релакс зеленый №1 494311\nПлитка декор 250х400х8 мм Релакс зеленый №2 494321\nПлитка декор 250х400х8 мм Релакс зеленый №3 494331\nПлитка декор 250х400х8 мм Релакс зеленый №4 494341\n\nПредназначена для использования в качестве декоративного элемента, применение которого позволяет украсить облицовываемую поверхность разнообразными узорами и орнаментами.\n\nТон (оттенок) может отличаться от партии к партии.\n\nСостав: огнеупорная глина, песок, известняк, пигменты.\n\nСтепень блеска: глянцевая\nКоличество в упаковке: 6 шт.\nПроизводитель: GOLDEN TILE, Украина\n\nСопутствующие: клей для плитки, крестики, затирка для швов, плиткорез.",
-    primaryImageUrl:
-      "//tdp.ru/images/p/cbb/ed1f4-8eb9-11e5-bed3-00259036a192.jpg",
-    assocProducts: "клей для плитки;\nкрестики;\nзатирка для швов;\nплиткорез;",
-    weight: 1430,
-    unit: "шт.",
-    unitFull: "штука",
-    unitRatio: 1,
-    unitAlt: "шт.",
-    unitRatioAlt: 1,
-    unitFullAlt: "штука",
     priceRetail: 627,
     priceRetailAlt: 627,
     priceGold: 609,
     priceGoldAlt: 609,
-    bonusAmount: 0,
-    hasAlternateUnit: true,
-    isActive: true,
-    modified: "2016-10-27 09:59:32",
   },
   {
     productId: "3236fe2d-09fe-11e6-bed3-00259036a192",
-    code: "00000156497",
-    title: "Плитка напольная 402х402х8,3 мм Уайтхолл черный (10 шт=1,62 кв.м)",
-    description:
-      "Предназначена для отделки пола в ванной комнате, санузлах, кухне и в других внутренних помещениях.\n\nТон (оттенок) может отличаться от партии к партии.\n\nСостав: огнеупорная глина, песок, известняк, пигменты. \n\nСтепень блеска: Матовая\nКоличество на поддоне:  48 коробок\nПроизводитель: KERAMA MARAZZI, Россия\n\nСопутствующие: клей для плитки, крестики, затирка для швов, плиткорез.\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n",
-    primaryImageUrl:
-      "//tdp.ru/images/p/fdb/98c28-5a3e-11e6-95f7-00259036a192.jpg",
-    assocProducts: "клей для плитки;\nкрестики;\nзатирка для швов;\nплиткорез;",
-    weight: 30500,
-    unit: "упак.",
-    unitFull: "упаковка",
-    unitRatio: 1,
-    unitAlt: "м. кв.",
-    unitRatioAlt: 0.6172,
-    unitFullAlt: "метр квадратный",
     priceRetail: 1197,
     priceRetailAlt: 738.7884,
     priceGold: 1149,
     priceGoldAlt: 709.1628,
-    bonusAmount: 0,
-    hasAlternateUnit: true,
-    isActive: true,
-    modified: "2016-10-27 10:04:42",
   },
   {
     productId: "da280b42-a23c-11e5-bed3-00259036a192",
-    code: "00000152640",
-    title:
-      "Плитка облицовочная 200х300х7 мм Каррара светло-палевая ЛАЙТ (18 шт = 1,08 кв.м)",
-    description:
-      "Предназначена для облицовки стен внутри жилых (кухня, ванная комната, туалет и пр.) и коммерческих (магазины, кафе, отели и пр.) помещений.\nРисунок на плитку нанесен методом цифровой печати., ее главное отличие отсутствие белого канта по периметру плитки.\n\nТон (оттенок) может отличаться от партии к партии.\n\nСостав: огнеупорная глина, кварцевый песок, пигменты.\n\nКоличество на поддоне: 80 коробок.\nПроизводитель: ЕВРО-КЕРАМИКА, Россия.\n\nСопутствующие: клей для плитки, клинья для кафеля, крестики, затирка для швов, плиткорез.",
-    primaryImageUrl:
-      "//tdp.ru/images/p/e20/1df2f-a895-11e5-bed3-00259036a192.jpg",
-    assocProducts:
-      "клей для плитки;\nклинья для кафеля;\nкрестики;\nзатирка для швов;\nплиткорез;",
-    weight: 13300,
-    unit: "упак.",
-    unitFull: "упаковка",
-    unitRatio: 1,
-    unitAlt: "м. кв.",
-    unitRatioAlt: 0.92592,
-    unitFullAlt: "метр квадратный",
     priceRetail: 230,
     priceRetailAlt: 212.9616,
     priceGold: 230,
     priceGoldAlt: 212.9616,
-    bonusAmount: 0,
-    hasAlternateUnit: true,
-    isActive: true,
-    modified: "2016-10-31 15:27:23",
   },
 ];
 
 document.addEventListener("DOMContentLoaded", () => {
   const productCards = document.querySelectorAll(".product_card");
+
   productCards.forEach((productCard) => {
     const priceTypeMeter = productCard.querySelector(
       ".product_price-type-meter"
@@ -237,6 +90,27 @@ document.addEventListener("DOMContentLoaded", () => {
         productPrice.innerHTML = formatPrice(product.priceRetailAlt);
       }
     });
+
+    const counter = productCard.querySelector(".product_card-count");
+    const minusBtn = productCard.querySelector(".product_card-btn--minus");
+    const plusBtn = productCard.querySelector(".product_card-btn--plus");
+
+    plusBtn.addEventListener("click", (e) => {
+      if (counter.value + 1 > 1) {
+        minusBtn.disabled = false;
+      }
+
+      counter.value = Number(counter.value) + 1;
+    });
+
+    minusBtn.addEventListener("click", (e) => {
+      if (counter.value - 1 <= 1) {
+        counter.value = Number(counter.value) - 1;
+        minusBtn.disabled = true;
+      } else {
+        counter.value = Number(counter.value) - 1;
+      }
+    });
   });
 
   function findProductById(id) {
@@ -245,43 +119,20 @@ document.addEventListener("DOMContentLoaded", () => {
 
   function formatPrice(price) {
     let newPrice = String(price);
+
     if (newPrice.includes(".")) {
       newPrice = newPrice.split(".");
+
       if (newPrice[1].length < 2) {
         newPrice[1] = newPrice[1].padEnd(2, "0");
       } else {
         newPrice[1] = newPrice[1].substr(0, 2);
       }
+
       return newPrice.join(".");
     }
+
     newPrice += ".00";
     return newPrice;
   }
-
-  const plusBtns = document.querySelectorAll(".product_card-btn--plus");
-  plusBtns.forEach((btn) => {
-    btn.addEventListener("click", (e) => {
-      const counter = e.target.parentNode.querySelector(".product_card-count");
-      if (counter.value + 1 > 1) {
-        const minusBtn = e.target.parentNode.querySelector(
-          ".product_card-btn--minus"
-        );
-        minusBtn.disabled = false;
-      }
-      counter.value = Number(counter.value) + 1;
-    });
-  });
-
-  const minusBtns = document.querySelectorAll(".product_card-btn--minus");
-  minusBtns.forEach((btn) => {
-    btn.addEventListener("click", (e) => {
-      const counter = e.target.parentNode.querySelector(".product_card-count");
-      if (counter.value - 1 <= 1) {
-        counter.value = Number(counter.value) - 1;
-        btn.disabled = true;
-      } else {
-        counter.value = Number(counter.value) - 1;
-      }
-    });
-  });
 });
